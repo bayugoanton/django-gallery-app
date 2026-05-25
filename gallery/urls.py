ccecac422b1860e6e558d49eec68dbfdea35e5fe
Anton Bayugo <bayugo.anton@gmai.com>
2026-05-25 22:32:52 +0800

    commit

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
+    
     # Custom sign-up route
     path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
 
